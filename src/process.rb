42eed2dad093d7bf84b2055d94a86d90ee1691d3
Rahul Gopinath <rahul@gopinath.org>
2016-04-25 15:20:48 -0700

    add length of test cases

--- src/process.rb
+++ src/process.rb
@@ -1,6 +1,21 @@
 #!/usr/bin/env ruby
 require 'set'
 
+def len_tc(lines)
+  # vecs will contain 3 records.
+  vecs = lines.select {|l| l =~ /INFO: (MinLen|ORIGLEN):/ }.map do |l|
+    case l
+    when /INFO: ORIGLEN:(.*)/
+      $1.to_i
+    when /INFO: MinLen:(.*)/
+      $1.to_i
+    else
+      raise l
+    end
+  end
+  return vecs
+end
+
 def stmt_cov_tc(lines)
   # vecs will contain 3 records.
   vecs = lines.select {|l| l =~ /INFO: .*VECLINE:/ }.map do |l|
@@ -52,6 +67,10 @@ def mut_cov_tc(lines)
 end
 
 
+def calc_len(covs)
+  covs.inject(:+)
+end
+
 def calc_cov(covs)
   tcl = covs.transpose.map do |a|
     if a.inject(:+) > 0
@@ -67,6 +86,19 @@ def calc_mut(muts)
   Set.new(muts.flatten).length
 end
 
+def fix_l(d, k)
+  if d.length == 1
+    [d[0],d[0],d[0]]
+  elsif d.length == 2
+    [d[0],d[1],d[1]]
+  elsif d.length == 3
+    d
+  else
+    raise "#{k}: #{d.length}"
+  end
+end
+
+
 def fix_d(d, k)
   if d.length == 1
     [d[0],d[0],d[0]]
@@ -77,6 +109,11 @@ def fix_d(d, k)
   else
     raise "#{k}: #{d.length}"
   end
+end
+
+def len_ts(tsuite)
+  tcs = tsuite.map {|k,lines| fix_l(len_tc(lines), k) } #.select{|v| v.length != 0}
+  return [calc_len(tcs.map{|tc| tc[0]}),calc_len(tcs.map{|tc| tc[1]}),calc_len(tcs.map{|tc| tc[2]})]
 end
 
 
@@ -96,13 +133,13 @@ def mut_cov_ts(tsuite)
 end
 
 
-
-
 dir = ARGV[0]
 tsarr = Dir.glob("#{dir}/*.slog").map {|tc| [tc, File.readlines(tc).map(&:chomp)]}.select{|tcls| tcls[1].length >=20}
 
 tsuite = Hash[tsarr]
 
+puts "Length of test cases for three suites"
+p len_ts(tsuite)
 puts "Statement coverage for three suites"
 p stmt_cov_ts(tsuite)
 puts "Condition coverage for three suites"
